5bf69b9ff98b20fdf9a0a8452d7dcc94201e5ac1
zss <280299844@qq.com>
2018-09-21 14:37:29 +0800

    新增webview[学习1]

--- app/src/main/java/com/zss/myandroid/MainActivity.java
+++ app/src/main/java/com/zss/myandroid/MainActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.tencent.bugly.crashreport.CrashReport;
+import com.zss.myandroid.webview.WebViewActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnBugly;
     private Button btnAnimSet;
+    private Button btnWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnBugly = findViewById(R.id.btn_bugly);
-        btnBugly.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CrashReport.testJavaCrash();
-            }
-        });
-
         btnAnimSet = findViewById(R.id.btn_animation_set);
-        btnAnimSet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnWebView = findViewById(R.id.btn_webview);
+
+        btnBugly.setOnClickListener(this);
+        btnAnimSet.setOnClickListener(this);
+        btnWebView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_bugly:
+                CrashReport.testJavaCrash();
+                break;
+            case R.id.btn_animation_set:
                 startActivity(new Intent(MainActivity.this,AnimationSetActivity.class));
-            }
-        });
+                break;
+            case R.id.btn_webview:
+                startActivity(new Intent(MainActivity.this,WebViewActivity.class));
+                break;
+        }
     }
 
     //测试
